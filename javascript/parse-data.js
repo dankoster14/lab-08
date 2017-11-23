fd8f31d7026b0c27f31c386737ecefccb61c32ec
Daniel Koster <dankoster14@gmail.com>
2017-11-23 09:51:02 +0000

    created for in loop

--- javascript/parse-data.js
+++ javascript/parse-data.js
@@ -8,23 +8,29 @@
   /*
     EXERCISE 01:
     Rewrite this to make it *loop* through all the lecturers' names.
-	
+
 	Clue: use "for key in..." and try to work out the syntax
     (Note the ES6 'template literal' syntax - you can add HTML tags)
   */
-  
+
   // set theData here to an empty string
-  
+
+  let theData = " ";
+
+  for(i in obj.lecturers){
+    theData += `<li>${obj.lecturers[i].firstName} ${obj.lecturers[i].lastName}</li>`;
+  }
+
   // create a loop around this line with "for ... in" (look it up)_
-  let theData = `${obj.lecturers[1].firstName} ${obj.lecturers[1].lastName}`;
+
   // end the for loop here
-  
+
   // populates the chosen element's content with the data:
-  demoData.innerHTML = theData; 
+  demoData.innerHTML = theData;
 
   /*
     EXERCISE 02:
-    Once you have looped through all the data, present the 
+    Once you have looped through all the data, present the
     results on the page, styled with CSS.
   */
 }());
